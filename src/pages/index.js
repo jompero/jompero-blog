c0b1934acccab3aff5be82845c4a205e5b39650a
Dani Jompero <dani.jompero@gmail.com>
2021-03-13 22:36:43 +0200

    Adds node command for mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,14 @@
 import * as React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import styled from "styled-components"
+import PostThumbnail from "../components/postThumbnail"
 
-const StyledLink = styled(Link)`
-  background: none;
-`
-
-const BlogTitle = styled.h3`
-
-`
-
-const BlogDate = styled.span`
-  font-size: smaller;
-  color: #737373;
-  
-  @media (min-width: 768px) {
-    float: right;
-  }
-
-  @media (max-width: 768px) {
-    display: block;
-    font-size: 50%;
-  }
-`
-
-const Excerpt = styled.p`
-  
-`
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <h2>Posts</h2>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-              <BlogTitle>
-                <StyledLink to={node.fields.slug}>
-                  {node.frontmatter.title}
-                </StyledLink>
-                <BlogDate>
-                    {node.frontmatter.date}
-                  </BlogDate>
-              </BlogTitle>
-            <Excerpt>{node.excerpt}</Excerpt>
-          </div>
-        ))}
+      {data.allMarkdownRemark.edges.map(({ node }) => node.frontmatter.date && <PostThumbnail key={node.id} node={node}/>)}
     </Layout>
   )
 }
